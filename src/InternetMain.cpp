2878a28cc9f2c81e738086f942f825311c73a3f4
Nayan <geeknayan@gmail.com>
2026-05-20 01:53:06 +0530

    kjdskaj

--- src/InternetMain.cpp
+++ src/InternetMain.cpp
@@ -7,6 +7,10 @@
 #include "threading/IThreadPool.h"
 
 #include "WiFiHealthCheckerThread.h"
+#include "InternetHealthCheckerThread.h"
+/*#include "LocalServerThread.h"
+#include "CloudServerThread.h"
+#include "RequestManagerThread.h"*/
 
 
 /*--@Autowired--*/
@@ -18,11 +22,19 @@ Void StartBackgroundThreads() {
     // Create runnable threads
     IRunnablePtr wifiHealthCheckerThread = std::make_shared<WiFiHealthCheckerThread>();
 
+    IRunnablePtr internetHealthCheckerThread = std::make_shared<InternetHealthCheckerThread>();
+
     // Submit to thread pool
     Bool wifiHealthCheckerThreadOk = threadPool->Execute(wifiHealthCheckerThread);
+    Bool internetHealthCheckerThreadOk = threadPool->Execute(internetHealthCheckerThread);
+
+
 
     if (wifiHealthCheckerThreadOk) printf("[INFO] WiFiHealthCheckerThread submitted to pool\n");
     else       printf("[ERROR] Failed to submit WiFiHealthCheckerThread\n");
+
+    if (internetHealthCheckerThreadOk) printf("[INFO] InternetHealthCheckerThread submitted to pool\n");
+    else       printf("[ERROR] Failed to submit InternetHealthCheckerThread\n");
 }
 
 
