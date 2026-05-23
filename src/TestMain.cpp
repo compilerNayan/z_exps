9cc10adede3b5cc524f872bf97ceb4be31d292b2
Nayan <geeknayan@gmail.com>
2026-05-23 18:41:29 +0530

    jdksakjd

--- src/TestMain.cpp
+++ src/TestMain.cpp
@@ -3,6 +3,7 @@
 
 #include "HttpClientTester.h"
 #include "wifi/IWiFiManager.h"
+#include "clock/IClockSynchronizer.h"
 
 void RunAllTests() {
     HttpClientTester tester;
@@ -15,11 +16,17 @@ IWiFiManagerPtr wifiManager = Implementation<IWiFiManager>::type::GetInstance();
 
 extern "C" void app_main(void) {
     if (!wifiManager->Connect("Garfield", Optional<StdString>{"123Madhu$$"})) {
-        printf("[ERROR] hkhdsdsdssdsdsdsdsds to sdj to WiFi\n");
+        printf("[ERROR] hkhdsdsdssdsdsdsdsdsds to sdj to WiFi\n");
         return;
     }
     if (!wifiManager->WaitForConnection(10000) || !wifiManager->IsConnected()) {
         printf("[ERROR] WiFi cesdonnection failed\n");
+        return;
+    }
+
+    if (!Implementation<IClockSynchronizer>::type::GetInstance()->SyncIfNeeded(
+            "pool.ntp.org", 15000, 500, "UTC0")) {
+        printf("[ERROR] SNTP time sync failed\n");
         return;
     }
 
