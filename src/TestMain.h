119a657cc563c0d66afa8d07807c142ad87839e1
Nayan <geeknayan@gmail.com>
2026-05-23 15:43:41 +0530

    hjhhjhjhj

--- src/TestMain.h
+++ src/TestMain.h
@@ -2,6 +2,7 @@
 #ifdef JDJEJED
 
 #include "HttpClientTester.h"
+#include "wifi/IWiFiManager.h"
 
 void RunAllTests() {
     HttpClientTester tester;
@@ -9,7 +10,19 @@ void RunAllTests() {
 }
 
 
+/*--@Autowired--*/
+IWiFiManagerPtr wifiManager = Implementation<IWiFiManager>::type::GetInstance();
+
 extern "C" void app_main(void) {
+    if (!wifiManager->Connect("Garfield", Optional<StdString>{"123Madhu$$"})) {
+        printf("[ERROR] Failed to connect to WiFi\n");
+        return;
+    }
+    if (!wifiManager->WaitForConnection(10000) || !wifiManager->IsConnected()) {
+        printf("[ERROR] WiFi cesdonnection failed\n");
+        return;
+    }
+
     RunAllTests();
 }
 
