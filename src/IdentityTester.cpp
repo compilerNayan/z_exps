92a8ca522232e4450f4f536e281dcbdceaf0fb8f
Nayan <geeknayan@gmail.com>
2026-05-30 17:27:40 +0530

    kjdska

--- src/IdentityTester.cpp
+++ src/IdentityTester.cpp
@@ -3,17 +3,36 @@
 
 #include "service/IDeviceService.h"
 #include "fleet/IFleetProvisioningService.h"
+#include "wifi/IWiFiManager.h"
+#include "clock/IClockSynchronizer.h"
+#include "Thread.h"
 
-/* @Autowired */
-IDeviceServicePtr deviceService;
+/*--@Autowired--*/
+IWiFiManagerPtr wifiManager = Implementation<IWiFiManager>::type::GetInstance();
 
-
-/* @Autowired */
-IFleetProvisioningServicePtr fleetProvisioningService;
+/*--@Autowired--*/
+IFleetProvisioningServicePtr fleetProvisioningService = Implementation<IFleetProvisioningService>::type::GetInstance();
 
 extern "C" void app_main(void) {
-    printf("IdentityTssester\n");
-    deviceService->Refresh();
+    if (!wifiManager->Connect("Garfield", Optional<StdString>{"123Madhu$$"})) {
+        printf("[ERROR] fj djsh sdj to WiFi\n");
+        return;
+    }
+    if (!wifiManager->WaitForConnection(10000) || !wifiManager->IsConnected()) {
+        printf("[ERROR] WiFi dsdsdssdsds failed\n");
+        return;
+    } else {
+        printf("[INFO] WiFi dskal successfully\n");
+    }
+
+    Thread::Sleep(3000);
+
+    if (!Implementation<IClockSynchronizer>::type::GetInstance()->SyncIfNeeded(
+            "pool.ntp.org", 15000, 500, "UTC0")) {
+        printf("[ERROR] SNTP time sync failed\n");
+        return;
+    }
+    fleetProvisioningService->EnrollDevice();
 }
 
 #endif // IKSKSKHFHFH
